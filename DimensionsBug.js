f42d2991f518076b1f71e732f586591ebf4057ff
BugHunter-X <askzenai@gmail.com>
2024-12-28 08:24:04 +0100

    Add DimensionsBug.js file

---
+++ DimensionsBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `Dimensions` API in React Native to get screen dimensions, but the dimensions are not yet available when the component mounts. This can lead to unexpected layout behavior or crashes.

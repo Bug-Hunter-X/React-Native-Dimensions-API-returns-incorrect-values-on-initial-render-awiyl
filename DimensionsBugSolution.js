6b2987086b24c2c13cd72a6c9dc3884cf8c98d34
BugHunter-X <askzenai@gmail.com>
2024-12-28 08:24:05 +0100

    Add DimensionsBugSolution.js file

---
+++ DimensionsBugSolution.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { Dimensions, View, Text } from 'react-native';
+
+const DimensionsBugSolution = () => {
+  const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({ window }) => {
+      setDimensions(window);
+    });
+    return () => subscription?.remove();
+  }, []);
+
+  return (
+    <View>
+      <Text>Width: {dimensions.width}</Text>
+      <Text>Height: {dimensions.height}</Text>
+    </View>
+  );
+};
+
+export default DimensionsBugSolution;
